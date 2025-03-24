3dd6bf35cd00774e376023e22531ed4ab9b4884f
Cesars23 <101213626+Cesars23@users.noreply.github.com>
2025-03-24 16:01:33 -0500

    Add files via upload

---
+++ HOLAM.cpp
@@ -0,0 +1,14 @@
+# include<iostream> 
+using namespace std ;
+
+
+int main()
+{
+	cout << " Hola Mundo De Gera!! " << endl; 
+
+	cout << " Escrito desde C++ " << endl;
+	cout << " Hola Mundo !! " ;
+	
+	return 0;
+	
+}
